fb051861b5c47f55e8ed5f94aebf36ae4391ddef
ktomkow <krzysztof.tomkow@gmail.com>
2021-09-29 22:14:18 +0200

    Generate random articles

--- SamplesServer/Controllers/ArticlesController.cs
+++ SamplesServer/Controllers/ArticlesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SamplesServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,9 +14,49 @@ namespace SamplesServer.Controllers
         [ProducesResponseType(200, Type = typeof(IEnumerable<Article>))]
         public async Task<IActionResult> Get()
         {
-            await Task.CompletedTask;
+            var random = new Random();
+            await Task.Delay(random.Next(1, 100));
 
-            return Ok();
+            var result = Article.GetRandom();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{count}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Article>))]
+        public async Task<IActionResult> Get(int count)
+        {
+            var random = new Random();
+            await Task.Delay(random.Next(1, 100));
+
+            var list = new List<Article>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(Article.GetRandom());
+            }
+
+            return Ok(list);
+        }
+
+        [HttpGet]
+        [Route("specific")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Article>))]
+        public async Task<IActionResult> GetSpecific()
+        {
+            var random = new Random();
+            await Task.Delay(random.Next(1, 100));
+
+            var result = new Article()
+            {
+                Id = 21,
+                Author = "John Doe Junior",
+                Title = "Loremus ipsumus wizardus",
+                DomainMonthlySubscribtion = 19.99m,
+                Link = "https://fake-address.com.org"
+            };
+
+            return Ok(result);
         }
     }
 }
--- SamplesServer/Models/Article.cs
+++ SamplesServer/Models/Article.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Bogus;
 
 namespace SamplesServer.Models
 {
     public class Article
     {
+        private static int id = 0;
+        private static Faker<Article> articleFaker = new Faker<Article>()
+                .StrictMode(true)
+                .RuleFor(x => x.Id, f => id++)
+                .RuleFor(x => x.Title, f => f.Commerce.Product())
+                .RuleFor(x => x.Author, f => f.Person.FullName)
+                .RuleFor(x => x.Link, f => "https://articles.com/article/" + f.Random.Number(1, 1000000).ToString())
+                .RuleFor(x => x.DomainMonthlySubscribtion, f => f.Finance.Amount(0, 100));
+
         public int Id { get; set; }
 
         public string Title { get; set; }
@@ -16,5 +22,10 @@ namespace SamplesServer.Models
         public string Link { get; set; }
 
         public decimal DomainMonthlySubscribtion { get; set; }
+
+        public static Article GetRandom()
+        {
+            return articleFaker.Generate();
+        }
     }
 }
